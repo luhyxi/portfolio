fcdbc4479567e0a78a74f17ebabda9129f810e1d
luana <luhyxi@gmail.com>
2024-10-05 20:45:37 -0300

    feat: creating display text

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,22 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar.vue'
+
 </script>
 
 <template>
-  <HelloWorld msg="Vite + Vue" />
+  <header>
+  <Navbar />
+  </header>
+
+  <main class="flex items-center justify-center min-h-screen bg-gray-100">
+        <div class="flex flex-col justify-center items-center">
+            <h2 class="text-3xl font-bold text-blue">Hello there,I'm </h2>
+            <h2 class="text-6xl p-10 font-bold">Luana Ritter.</h2>
+            <h2 class="text-xl">
+              <span class="font-bold">Fullstack Developer</span> and stuff aficionado
+            </h2>
+        </div>
+    </main>
 </template>
+
 
---
+++ src/components/DisplayText.vue
@@ -0,0 +1,18 @@
+<template>
+    <main class="flex items-center justify-center min-h-screen bg-gray-100">
+        <div class="p-6 bg-white rounded-lg shadow-md">
+            <h1 class="text-xl font-bold">Centered Content</h1>
+            <p class="mt-2 text-gray-600">This is a centered div inside a main element.</p>
+        </div>
+    </main>
+</template>
+
+<script>
+export default {
+    name: "CenteredDiv",
+};
+</script>
+
+<style scoped>
+/* Add any additional styles here */
+</style>
